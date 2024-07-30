cdb1ddcb65b2b02c15bec19eba7b421b15536900
ivanzmilic <ivanzmilic@gmail.com>
2024-07-30 15:25:00 +0200

    More transformation functions

---
+++ sir_to_snapi.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from astropy.io import fits
+import pyana
+import sys
+
+# Transforms a single atmospheric cube from cobold h5py format into f0 (pyana) snapi format
+
+cobfile = sys.argv[1]
+
+atmin = fits.open(cobfile)[0].data[:,:,:768,:768]
+
+NP, NZ, NX, NY = atmin.shape
+
+print ("info :: dimensions are: ", NX, NY, NZ)
+
+atmin = atmin.transpose(0,2,3,1)
+
+atmout = np.zeros([12, NX, NY, NZ])
+
+atmout[0,:,:,:] = atmin[0,:,:,:]
+atmout[1,:,:,:] = atmin[9,:,:,:]
+atmout[2,:,:,:] = atmin[1,:,:,:]
+atmout[3,:,:,:] = atmin[2,:,:,:]
+atmout[9,:,:,:] = atmin[5,:,:,:]*(-1.0)
+atmout[7,:,:,:] = atmin[4,:,:,:]
+atmout[10,:,:,:] = np.radians(atmin[6,:,:,:])
+atmout[11,:,:,:] = np.radians(atmin[7,:,:,:])
+
+pyana.fzwrite(sys.argv[2],atmout[:,:,:,::-1],0,'temp')
