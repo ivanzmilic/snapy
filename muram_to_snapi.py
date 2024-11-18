db5260edebaf29949a32e2b4499e53a6ffbf5369
ivanzmilic <ivanzmilic@gmail.com>
2024-11-18 15:40:50 +0100

    slight modification

--- muram_to_snapi.py
+++ muram_to_snapi.py
@@ -3,36 +3,37 @@ import matplotlib.pyplot as plt
 import pyana
 import h5py 
 import sys
+import muram as mio
 
-# Transforms a single atmospheric cube from cobold h5py format into f0 (pyana) snapi format
+# Reads a single muram snapshot and packs it into a .f0 snapi atmosphere: 
 
-cobfile = sys.argv[1]
+path = sys.argv[1]
+it_no = int(sys.argv[2])
 
-atmin = h5py.File(cobfile,'r')
+snap=muram.MuramSubSnap(path,it_no)
 
-print ("info:: file keys are: ", atmin.keys())
+T = snap.Temp.transpose(1,2,0)
+	
+	print ("info::muram_binary_loader::the original dimensions are: ", T.shape)
 
-NX = len(atmin['x'])
-NY = len(atmin['y'])
-NZ = len(atmin['z'])
-
+NX, NY, NZ = T.shape
 print ("info :: dimensions are: ", NX, NY, NZ)
 
 atmout = np.zeros([12, NX, NY, NZ])
 
-atmout[0,:,:,:] = np.linspace(-6,2,NZ)[None,None,:]
-z = atmin['z'][:]
-atmout[1,:,:,:] = z[None,None,:]
-atmout[2,:,:,:] = atmin['T'][:,:,:].transpose(2,1,0)
-atmout[3,:,:,:] = atmin['p'][:,:,:].transpose(2,1,0)
-atmout[9,:,:,:] = atmin['vz'][:,:,:].transpose(2,1,0)
+#atmout[0,:,:,:] = np.linspace(-6,2,NZ)[None,None,:]
+#z = atmin['z'][:]
+#atmout[1,:,:,:] = z[None,None,:]
+#atmout[2,:,:,:] = atmin['T'][:,:,:].transpose(2,1,0)
+#atmout[3,:,:,:] = atmin['p'][:,:,:].transpose(2,1,0)
+#atmout[9,:,:,:] = atmin['vz'][:,:,:].transpose(2,1,0)
 
-B_mag = np.sqrt(atmin['bz'][:,:,:]**2.0 + atmin['bx'][:,:,:]**2.0 + atmin['by'][:,:,:]**2.0)
-theta = np.arccos(atmin['bz'][:,:,:]/(B_mag[:,:,:]+0.1)) # make sure it's not dividing by zero
-phi   = np.arctan(atmin['by'][:,:,:] / atmin['bx'][:,:,:])
+#B_mag = np.sqrt(atmin['bz'][:,:,:]**2.0 + atmin['bx'][:,:,:]**2.0 + atmin['by'][:,:,:]**2.0)
+#theta = np.arccos(atmin['bz'][:,:,:]/(B_mag[:,:,:]+0.1)) # make sure it's not dividing by zero
+#phi   = np.arctan(atmin['by'][:,:,:] / atmin['bx'][:,:,:])
 
-atmout[7,:,:,:] = B_mag.transpose(2,1,0)
-atmout[10,:,:,:] = theta.transpose(2,1,0)
-atmout[11,:,:,:] = phi.transpose(2,1,0)
+#atmout[7,:,:,:] = B_mag.transpose(2,1,0)
+#atmout[10,:,:,:] = theta.transpose(2,1,0)
+#atmout[11,:,:,:] = phi.transpose(2,1,0)
 
 pyana.fzwrite(sys.argv[2],atmout[:,:,:,::-1],0,'temp')
