419f586c660d0f4588e2024bc28dd9be8023ef79
milic <ivanzmilic@gmail.com>
2024-12-19 16:50:57 +0100

    Changed routine to transform muram cube into a snapi cube.
    Added an example of jupyter notebook.

--- muram_to_snapi.py
+++ muram_to_snapi.py
@@ -7,28 +7,8 @@ import muram as mio
 # Reads a single muram snapshot and packs it into a .f0 snapi atmosphere: 
 
 path = sys.argv[1]
-it_no = int(sys.argv[2])
-
-snap=mio.MuramSubSnap(path,it_no)
-
-T = snap.Temp.transpose(1,2,0)
-	
-print ("info::muram_binary_loader::the original dimensions are: ", T.shape)
-
-NX, NY, NZ = T.shape
-print ("info :: dimensions are: ", NX, NY, NZ)
-
-# Transforms a muram snapshot, read from the path and iteration number into a 12-parameter snapi .f0 cube
-path = sys.argv[1]
 n_iter = int(sys.argv[2])
-
-snap=mio.MuramSubSnap(path,n_iter)
-
-T = snap.Temp.transpose(1,2,0)
-
-NX, NY, NZ = T.shape
-
-print ("info :: original dimensions are: ", NX, NY, NZ)
+type = sys.argv[10]
 
 xmin = int(sys.argv[3])
 xmax = int(sys.argv[4])
@@ -38,40 +18,100 @@ zmin = int(sys.argv[7])
 zmax = int(sys.argv[8])
 skip = int(sys.argv[9])
 
-T = snap.Temp[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+if (type=='muramsub'):
 
-NX, NY, NZ = T.shape
-print ("info :: output dimensions are: ", NX, NY, NZ)
+	snap=mio.MuramSubSnap(path,n_iter)
 
-atmout = np.zeros([12,NX,NY,NZ])
-Tc = np.copy(T)
-Tc[np.where(T<3000.0)] = 3000.0
-#Tc[np.where(T>50000.0)] = 50000.0
-z = np.arange(zmax-zmin) * 16E5
-tau = np.linspace(-6,2,NZ)
-p = snap.Pres[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
-vz = snap.vx[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
-
-atmout[0,:,:,:] = tau[None, None, :]
-atmout[1,:,:,:] = z[None, None, :]
-atmout[2,:,:,:] = Tc
-atmout[3,:,:,:] = p
-atmout[4,:,:,:] = p * 0.05
-atmout[9,:,:,:] = vz
-
-Bz = snap.Bx[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
-Bx = snap.By[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
-By = snap.Bz[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	T = snap.Temp.transpose(1,2,0)
 	
-B = np.sqrt(Bx**2.0 + By**2.0 + Bz**2.0) * np.sqrt(4.0*np.pi)
-theta = np.arccos(Bz/(B+0.001))
-phi = np.arctan(By/Bx)
+	print ("info::muram_binary_loader::the original dimensions are: ", T.shape)
 
-atmout[7,:,:,:] = B
-atmout[10,:,:,:] = theta
-atmout[11,:,:,:] = phi
+	NX, NY, NZ = T.shape
+	
+	print ("info :: original dimensions are: ", NX, NY, NZ)
 
-outputname = sys.argv[10] + '_' + sys.argv[2]+ '.f0'
+	T = snap.Temp[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
 
-pyana.fzwrite(outputname, atmout[:,:,:,::-1],0,'bla')
+	NX, NY, NZ = T.shape
+	print ("info :: output dimensions are: ", NX, NY, NZ)
 
+	atmout = np.zeros([12,NX,NY,NZ])
+	Tc = np.copy(T)
+	Tc[np.where(T<3200.0)] = 3200.0
+	#Tc[np.where(T>50000.0)] = 50000.0
+	z = np.arange(zmax-zmin) * 16E5
+	tau = np.linspace(-6,2,NZ)
+	p = snap.Pres[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	vz = snap.vx[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+
+	atmout[0,:,:,:] = tau[None, None, :]
+	atmout[1,:,:,:] = z[None, None, :]
+	atmout[2,:,:,:] = Tc
+	atmout[3,:,:,:] = p
+	atmout[4,:,:,:] = p * 0.05
+	atmout[9,:,:,:] = vz
+
+	Bz = snap.Bx[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	Bx = snap.By[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	By = snap.Bz[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	
+	B = np.sqrt(Bx**2.0 + By**2.0 + Bz**2.0) * np.sqrt(4.0*np.pi)
+	theta = np.arccos(Bz/(B+0.001))
+	phi = np.arctan(By/Bx)
+
+	atmout[7,:,:,:] = B
+	atmout[10,:,:,:] = theta
+	atmout[11,:,:,:] = phi
+
+	outputname = sys.argv[10] + '_' + sys.argv[2]+ '.f0'
+
+	pyana.fzwrite(outputname, atmout[:,:,:,::-1],0,'bla')
+
+elif (type=='muram'):
+
+	snap=mio.MuramSnap(path,n_iter)
+
+	T = snap.Temp.transpose(1,2,0)
+	
+	print ("info::muram_binary_loader::the original dimensions are: ", T.shape)
+
+	NX, NY, NZ = T.shape
+	
+	print ("info :: original dimensions are: ", NX, NY, NZ)
+
+	T = snap.Temp[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+
+	NX, NY, NZ = T.shape
+	print ("info :: output dimensions are: ", NX, NY, NZ)
+
+	atmout = np.zeros([12,NX,NY,NZ])
+	Tc = np.copy(T)
+	Tc[np.where(T<3200.0)] = 3200.0
+	#Tc[np.where(T>50000.0)] = 50000.0
+	z = np.arange(zmax-zmin) * 16E5
+	tau = np.linspace(-6,2,NZ)
+	p = snap.Pres[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	vz = snap.vx[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+
+	atmout[0,:,:,:] = tau[None, None, :]
+	atmout[1,:,:,:] = z[None, None, :]
+	atmout[2,:,:,:] = Tc
+	atmout[3,:,:,:] = p
+	atmout[4,:,:,:] = p * 0.05
+	atmout[9,:,:,:] = vz
+
+	Bz = snap.Bx[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	Bx = snap.By[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	By = snap.Bz[zmin:zmax, xmin:xmax:skip, ymin:ymax:skip].transpose(1,2,0)
+	
+	B = np.sqrt(Bx**2.0 + By**2.0 + Bz**2.0) * np.sqrt(4.0*np.pi)
+	theta = np.arccos(Bz/(B+0.001))
+	phi = np.arctan(By/Bx)
+
+	atmout[7,:,:,:] = B
+	atmout[10,:,:,:] = theta
+	atmout[11,:,:,:] = phi
+
+	outputname = sys.argv[11] + '_' + sys.argv[2]+ '.f0'
+
+	pyana.fzwrite(outputname, atmout[:,:,:,::-1],0,'bla')	
